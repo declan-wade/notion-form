e674dd1667d623a1ce9f325587c9f03408b60f11
declan-wade <17171018+declan-wade@users.noreply.github.com>
2024-01-11 10:17:29 +0800

    Update route.ts

--- src/app/actions/handleUpload/route.ts
+++ src/app/actions/handleUpload/route.ts
@@ -5,9 +5,14 @@ export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get('filename');
  
+  if (filename !== null && request.body !== null) {
   const blob = await put(filename, request.body, {
     access: 'public',
   });
  
   return NextResponse.json(blob);
+} else {
+  // Handle the case when 'filename' is null
+  return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+}
 }
