288d5ddb789fa38b0c0a0b558cb8989d6e790bfb
declan-wade <17171018+declan-wade@users.noreply.github.com>
2024-01-11 10:04:27 +0800

    Initial commit

---
+++ src/app/actions/handleSubmit.ts
@@ -0,0 +1,31 @@
+"use server";
+
+const { Client } = require("@notionhq/client");
+
+// Initializing Notion client
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+  
+  export async function addTodo (formData: FormData){
+    const title = formData.get('title');
+    const slug = formData.get('slug');
+    try {
+      const databaseId = process.env.NOTION_DB_ID;
+      const response = await notion.pages.create({
+        "parent": {
+          "type": "database_id",
+          "database_id": databaseId
+        },
+        "properties": {
+          "title": {"title": [{ "type": "text", "text": { "content": title } }]},
+          "slug": {"rich_text": [{"text": {"content": slug }}]},
+        }
+      });
+      console.log(response);
+      return {message:"Success"};
+    } catch (error) {
+        console.log(error)
+        return {message: "Error"}
+    }
+  };
---
+++ src/app/page.tsx
@@ -0,0 +1,150 @@
+"use client";
+import styles from "./page.module.css";
+import Button from "@mui/material/Button";
+import AppBar from "@mui/material/AppBar";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Avatar from "@mui/material/Avatar";
+import * as React from "react";
+import { addTodo } from "./actions/handleSubmit";
+import Snackbar from "@mui/material/Snackbar";
+import Container from "@mui/material/Container";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import { revalidatePath } from "next/cache";
+import type { PutBlobResult } from '@vercel/blob';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function Home() {
+  const inputFileRef = React.useRef<HTMLInputElement>(null);
+  const [blob, setBlob] = React.useState<PutBlobResult | null>(null);
+  const [openSuccess, setOpenSuccess] = React.useState(false);
+  const [openError, setOpenError] = React.useState(false);
+  const [submitButton, setSubmitButton] = React.useState(false);
+
+  async function clientAction(formData: FormData) {
+    setSubmitButton(true);
+    const response = await addTodo(formData);
+    if (response.message === "Success") {
+      setOpenSuccess(true);
+      setSubmitButton(false);
+      window.location.assign("/success");
+    } else {
+      setOpenError(true);
+      setSubmitButton(false);
+    }
+  }
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Snackbar open={openError} autoHideDuration={6000}>
+        <Alert severity="error" sx={{ width: "100%" }}>
+          This is an error message!
+        </Alert>
+      </Snackbar>
+      <Snackbar open={openSuccess} autoHideDuration={6000}>
+        <Alert severity="success" sx={{ width: "100%" }}>
+          Form submitted successfully!
+        </Alert>
+      </Snackbar>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <form action={clientAction}>
+            <TextField
+              id="title"
+              name="title"
+              fullWidth
+              label="Title"
+              variant="outlined"
+            />
+            <br></br>
+            <br></br>
+            <TextField
+              id="slug"
+              name="slug"
+              fullWidth
+              label="Slug"
+              variant="outlined"
+            />
+            <br></br>
+            <br></br>
+            <Button type="submit" variant="outlined" disabled={submitButton}>
+              Submit
+            </Button>
+          </form>
+        </Box>
+        <Box sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}>
+        <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+ 
+          if (!inputFileRef.current?.files) {
+            throw new Error('No file selected');
+          }
+ 
+          const file = inputFileRef.current.files[0];
+ 
+          const response = await fetch(
+            `/actions/handleUpload?filename=${file.name}`,
+            {
+              method: 'POST',
+              body: file,
+            },
+          );
+ 
+          const newBlob = (await response.json()) as PutBlobResult;
+ 
+          setBlob(newBlob);
+        }}
+      >
+        <input name="file" ref={inputFileRef} type="file" required />
+        <button type="submit">Upload</button>
+      </form>
+
+        </Box>
+      </Container>
+    </div>
+  );
+}
